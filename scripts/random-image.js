881bb4b4c9afbca1e7f49b0f74b7bb10994c0936
HandwerSTD <1875876967@qq.com>
2020-10-16 21:05:13 +0800

    add custom random image list

--- scripts/random-image.js
+++ scripts/random-image.js
@@ -5,8 +5,18 @@ module.exports.randomImage = function (hexo) {
     var config = hexo.theme.config;
     var data = hexo.locals.get('data');
     var res = config.post_image.default;
-    if (config.post_image.random && data.galleries) {
+    if (config.post_image.random == 'gallery' && data.galleries) {
       var galleries = data.galleries;
+      var images = [];
+      for (var item of galleries) {
+        item.photos.forEach(function (item) {
+          images.push(item);
+        });
+      }
+      var count = images.length;
+      res = images[Math.floor(Math.random() * count)];
+    } else if (config.post_image.random == 'local' && data.local_image) {
+      var galleries = data.local_image;
       var images = [];
       for (var item of galleries) {
         item.photos.forEach(function (item) {
