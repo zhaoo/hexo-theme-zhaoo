14ef3d219cc341ec9cfe169088426ece2177dc6b
HandwerSTD <1875876967@qq.com>
2020-10-17 18:29:55 +0800

    add post image id

--- scripts/entry.js
+++ scripts/entry.js
@@ -4,4 +4,5 @@ require("./merge-config").mergeConfig(hexo);
 require("./lazyload").lazyload(hexo);
 require("./title").title(hexo);
 require("./carrier").carrier(hexo);
-require("./random-image").randomImage(hexo);
+require("./post-image").randomImage(hexo);
+require("./post-image").getImageById(hexo);
---
+++ scripts/post-image.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports.randomImage = function (hexo) {
+  hexo.extend.helper.register('random_image', function () {
+    var config = hexo.theme.config;
+    var data = hexo.locals.get('data');
+    var res = config.post_image.default;
+    if (config.post_image.random == 'gallery' && data.galleries) {
+      var galleries = data.galleries;
+      var images = [];
+      for (var item of galleries) {
+        item.photos.forEach(function (item) {
+          images.push(item);
+        });
+      }
+      var count = images.length;
+      res = images[Math.floor(Math.random() * count)];
+    } else if (config.post_image.random == 'local' && data.local_image) {
+      var galleries = data.local_image;
+      var images = [];
+      for (var item of galleries) {
+        item.photos.forEach(function (item) {
+          images.push(item);
+        });
+      }
+      var count = images.length;
+      res = images[Math.floor(Math.random() * count)];
+    }
+    return res;
+  });
+}
+
+module.exports.getImageById = function (hexo) {
+  hexo.extend.helper.register('getImageById', function (id) {
+    var config = hexo.theme.config;
+    var data = hexo.locals.get('data');
+    var res = config.post_image.default;
+    if (config.post_image.random == 'gallery' && data.galleries) {
+      var galleries = data.galleries;
+      var images = [];
+      for (var item of galleries) {
+        item.photos.forEach(function (item) {
+          images.push(item);
+        });
+      }
+      var count = images.length;
+      res = images[(id - 1) % count];
+    } else if (config.post_image.random == 'local' && data.local_image) {
+      var galleries = data.local_image;
+      var images = [];
+      for (var item of galleries) {
+        item.photos.forEach(function (item) {
+          images.push(item);
+        });
+      }
+      var count = images.length;
+      res = images[(id - 1) % count];
+    }
+    return res;
+  });
+}
